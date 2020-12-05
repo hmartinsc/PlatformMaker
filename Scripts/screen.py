e7903357b63266fd4748cc3c0a99107dd9715e90
APF2000 <arthurpfonseca@gmail.com>
2020-12-04 21:28:10 -0300

    Tela maior

--- Scripts/screen.py
+++ Scripts/screen.py
@@ -8,7 +8,7 @@ class Screen():
 		self.layout = params['layout']() # Execute function of layout
 		self.title = params['title']
 		
-		self.window = sg.Window(self.title, self.layout) # Window Defintion
+		self.window = sg.Window(self.title, self.layout, size=(800,600)) # Window Defintion
 
 		self.window_behaviour = params['behaviour'] # Behaviour -> retorna status da tela
 		
@@ -19,11 +19,11 @@ class Screen():
 	def close(self):
 		self.window.close()
 		
-	
-next_screen = 'main'
-behavs = screen_behaviour.behaviours
-while next_screen:
-	print('Next screen: ', next_screen)
-	s = Screen(behavs[next_screen])
-	next_screen = s.display()
-	s.close()
+if __name__ == '__main__':
+	next_screen = 'main'
+	behavs = screen_behaviour.behaviours
+	while next_screen:
+		print('Next screen: ', next_screen)
+		s = Screen(behavs[next_screen])
+		next_screen = s.display()
+		s.close()
