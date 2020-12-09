7e10ce4bdcb90296c3ecccd864d15ff58e63ab33
Lucas Leme <lucaslssantos99@gmail.com>
2020-12-08 21:39:24 -0300

    Identação e add_user com senha

--- Scripts/database.py
+++ Scripts/database.py
@@ -1,58 +1,65 @@
 import pg8000
 import psswd
 
-conn = pg8000.connect(user = 'postgres',
-	host = 'localhost',
-	password = psswd.psswd,
-	database = 'jogo')
-	
+conn = pg8000.connect(user='postgres',
+                      host='localhost',
+                      password=psswd.psswd,
+                      database='jogo')
+
 conn.autocommit = True
 cursor = conn.cursor()
 
-class Screen():
-		
-	def __init__(self, params):
-		self.layout = params['layout']() # Execute function of layout
-		self.title = params['title']
-		
-		self.window = sg.Window(self.title, self.layout, size=(800,600)) # Window Defintion
 
-		self.window_behaviour = params['behaviour'] # Behaviour -> retorna status da tela
-		
-	def display(self):
-		#print('Displaying screen')
-		return self.window_behaviour( self.window )
-		
-	def close(self):
-		self.window.close()
+class Screen():
+
+    def __init__(self, params):
+        self.layout = params['layout']()  # Execute function of layout
+        self.title = params['title']
+
+        self.window = sg.Window(self.title, self.layout,
+                                size=(800, 600))  # Window Defintion
+
+        # Behaviour -> retorna status da tela
+        self.window_behaviour = params['behaviour']
+
+    def display(self):
+        #print('Displaying screen')
+        return self.window_behaviour(self.window)
+
+    def close(self):
+        self.window.close()
+
 
 def exe_query(query=str()):
-	cursor.execute(query)
-	try:
-		response = cursor.fetchall()
-	except pg8000.exceptions.ProgrammingError:
-		return cursor.rowcount
-	
-	return(response)
-	
+    cursor.execute(query)
+    try:
+        response = cursor.fetchall()
+    except pg8000.exceptions.ProgrammingError:
+        return cursor.rowcount
+
+    return(response)
+
+
 def user_exists(username, email):
-	query = "SELECT * FROM public.jogador WHERE email = '%s' AND senha = '%s'"
-	print(query % (username, email))
-	#import pdb; pdb.set_trace()
+    query = "SELECT * FROM public.jogador WHERE email = '%s' AND senha = '%s'"
+    print(query % (username, email))
+    #import pdb; pdb.set_trace()
 
-	result = exe_query(query % (username, email))
-	return result
+    result = exe_query(query % (username, email))
+    return result
 
-	
-def add_user(username, email):
-	# Verify if is valid!!!!!
-	
-	# Add to database
-	query = "INSERT INTO  public.jogador (nick, email) VALUES ('%s', '%s')" % (username, email)
-	exe_query(query)
+
+def add_user(username, email, password):
+    # Verify if is valid!!!!!
+
+    # Add to database
+    query = "INSERT INTO  public.jogador (nick, email, senha) VALUES ('%s', '%s', '%s')" % (
+        username, email, password)
+    exe_query(query)
+
 
 def list_friends(email):
-	query = '''
+    query = '''
 		WITH l_amizades AS 
 		(
 			SELECT j1.nick n1, j1.email em1, j2.nick n2, j2.email em2
@@ -66,11 +73,12 @@ def list_friends(email):
 		UNION
 		SELECT n2 FROM l_amizades WHERE em1 = '%s'
 	'''
-	print(query % (email, email))
-	return exe_query(query % (email, email))
+    print(query % (email, email))
+    return exe_query(query % (email, email))
+
 
 def add_friend(email1, email2):
-	query = '''
+    query = '''
 		INSERT INTO public.Amizade
 		VALUES
 		(
@@ -86,30 +94,30 @@ def add_friend(email1, email2):
 			)
 		)
 	'''
-	print(query % (email1, email2))
-	try:
-		exe_query(query % (email1, email2))
-	except pg8000.exceptions.IntegrityError:
-		print("AMigo ja adicionado")
-		pass # Adicionou quem ja era amigo
+    print(query % (email1, email2))
+    try:
+        exe_query(query % (email1, email2))
+    except pg8000.exceptions.IntegrityError:
+        print("AMigo ja adicionado")
+        pass  # Adicionou quem ja era amigo
 
 
 if __name__ == '__main__':
-	username = "noobmaster"
-	email = "haroldo.mansur@outlook.com"
+    username = "noobmaster"
+    email = "haroldo.mansur@outlook.com"
 
-	if user_exists(username, email):
-		print("User exists")
-	else:
-		print("User not found")
+    if user_exists(username, email):
+        print("User exists")
+    else:
+        print("User not found")
 
-	emails = [('haroldo.mansur@outlook.com'),\
-	('nino@gmail.com'),\
-	('numero4@knd.com'),\
-	('mamaefalei@gmail.com'),\
-	('horacio@panini.com.br'),\
-	('bolas@mtst.com.br'),\
-	('couvos.flor@psdb.gov.br')]
+    emails = [('haroldo.mansur@outlook.com'),
+              ('nino@gmail.com'),
+              ('numero4@knd.com'),
+              ('mamaefalei@gmail.com'),
+              ('horacio@panini.com.br'),
+              ('bolas@mtst.com.br'),
+              ('couvos.flor@psdb.gov.br')]
 
-	for el in emails:
-		add_friend(el, 'email')
+    for el in emails:
+        add_friend(el, 'email')
